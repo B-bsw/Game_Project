6c160b1670334277e7c47b4dbc00fc9f23eb3034
BB <sorawitthassadorn15@gmail.com>
2025-03-07 02:15:18 +0700

    update switch-page

--- game_levelDevil/src/main/java/game/game_leveldevil/HelloApplication.java
+++ game_levelDevil/src/main/java/game/game_leveldevil/HelloApplication.java
@@ -2,19 +2,27 @@ package game.game_leveldevil;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static Stage stageS;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stageS = stage;
+        switchPage("hello-view.fxml");
+        stage.setTitle("MyApp");
         stage.show();
+    }
+
+    public void switchPage(String x) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(x));
+        Scene scene = new Scene(fxmlLoader.load(),500,500);
+        stageS.setScene(scene);
     }
 
     public static void main(String[] args) {
