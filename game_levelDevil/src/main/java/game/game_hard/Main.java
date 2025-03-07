2990e8e4f81c8282da534a2a51ab0904407412a7
BB <sorawitthassadorn15@gmail.com>
2025-03-08 02:36:22 +0700

    fix at main

--- game_levelDevil/src/main/java/game/game_hard/Main.java
+++ game_levelDevil/src/main/java/game/game_hard/Main.java
@@ -14,29 +14,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("first.fxml"));
-            Parent root = loader.load();
-            scene = new Scene(root);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("first.fxml"));
+        Parent root = loader.load();
+        scene = new Scene(root);
 
-            Animation controller = loader.getController();
-            controller.initialize(scene);
+        Animation controller = loader.getController();
+        controller.initialize(scene);
 
-            stage.setScene(scene);
-            stage.setTitle("MyGame");
-            stage.show();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    static void setRoot(String fxml) throws IOException{
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml));
-        return fxmlLoader.load();
+        stage.setScene(scene);
+        stage.setTitle("MyGame");
+        stage.show();
     }
 
     public static void main(String[] args) {
