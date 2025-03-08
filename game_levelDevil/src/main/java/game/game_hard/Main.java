5e887ef82afb2e40fc597dcadb7ce1505c20cbe7
BB <sorawitthassadorn15@gmail.com>
2025-03-09 02:11:08 +0700

    Change Name on resources

--- game_levelDevil/src/main/java/game/game_hard/Main.java
+++ game_levelDevil/src/main/java/game/game_hard/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("first.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gate1.fxml"));
         Parent root = loader.load();
         scene = new Scene(root);
 
